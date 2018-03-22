cf7422dedf5068004e1bfcaf97812a2166328562
Jones <jones@jones-vostok.home>
2018-03-23 00:41:09 +0100

    add Dronecode-SDK-Swift to Source

---
+++ Source/Dronecode-SDK-Swift/Dronecode-SDK-SwiftTests/Dronecode_SDK_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Dronecode_SDK_SwiftTests.swift
+//  Dronecode-SDK-SwiftTests
+//
+//  Created by Jones on 22.03.18.
+//  Copyright © 2018 Dronecode. All rights reserved.
+//
+
+import XCTest
+@testable import Dronecode_SDK_Swift
+
+class Dronecode_SDK_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ Source/README.md
@@ -0,0 +1,12 @@
+# Building Dronecode-SDK-Swift
+
+## Install dependencies
+
+### Manually
+
+1. Download dependencies from [here](https://s3.eu-central-1.amazonaws.com/dronecode-sdk/dronecore-swift-prototype.zip).
+2. Unzip `dronecore-swift-prototype.zip`
+3. Open Dronecode-SDK-Swift.xcodeproj in Xcode
+4. Copy frameworks in the `Frameworks` group of Dronecode-SDK-Swift in Xcode
+
+Note: For now, the SDK is not built for the simulator.
