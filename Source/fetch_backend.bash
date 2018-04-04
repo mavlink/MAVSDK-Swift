b8cdc6c37b7d7b1a43077313ea0842b66ea8a576
Jonas Vautherin <jonas.vautherin@gmail.com>
2018-04-04 15:03:10 +0200

    add script to fetch archive from s3
    * improve fetch_backend.bash on the way

--- Source/fetch_backend.bash
+++ Source/fetch_backend.bash
@@ -6,5 +6,5 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source ${SCRIPT_DIR}/init_variables.bash
 
-wget https://s3.eu-central-1.amazonaws.com/dronecode-sdk/${ARCHIVE_BACKEND} -P ${TMP_DIR}
+curl https://s3.eu-central-1.amazonaws.com/dronecode-sdk/${ARCHIVE_BACKEND} --output ${TMP_DIR}/${ARCHIVE_BACKEND}
 unzip "${TMP_DIR}/${ARCHIVE_BACKEND}" -d ${BIN_DIR}
