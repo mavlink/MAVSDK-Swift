d0b959976d80567a537a0e4fb3f4607976208960
Jonas Vautherin <jonas.vautherin@gmail.com>
2018-04-05 14:08:33 +0200

    core: add timeout observable

--- Source/Dronecode-SDK-Swift/Dronecode-SDK-Swift/Core.swift
+++ Source/Dronecode-SDK-Swift/Dronecode-SDK-Swift/Core.swift
@@ -5,29 +5,29 @@ import RxSwift
 
 public class Core {
     let service: Dronecore_Rpc_Core_CoreServiceService
-    
+
     public convenience init(address: String, port: Int) {
         let service = Dronecore_Rpc_Core_CoreServiceServiceClient(address: "\(address):\(port)")
         self.init(service: service)
     }
-    
+
     init(service: Dronecore_Rpc_Core_CoreServiceService = Dronecore_Rpc_Core_CoreServiceServiceClient(address: "localhost:50051", secure: false)) {
         self.service = service
         
         gRPC.initialize()
     }
-    
+
     public func connect() {
         DispatchQueue.global(qos: .background).async {
             print("Running backend in background (MAVLink port: 14540)")
             runBackend(14540)
         }
     }
-    
+
     public func getDiscoverObservable() -> Observable<UInt64> {
         return Observable.create { observer in
             let discoverRequest = Dronecore_Rpc_Core_SubscribeDiscoverRequest()
-            
+
             do {
                 let call = try self.service.subscribediscover(discoverRequest, completion: nil)
                 while let response = try? call.receive() {
@@ -36,7 +36,24 @@ public class Core {
             } catch {
                 observer.onError("Failed to subscribe to discovery stream")
             }
-        
+
+            return Disposables.create()
+        }
+    }
+
+    public func getTimeoutObservable() -> Observable<Void> {
+        return Observable.create { observer in
+            let timeoutRequest = Dronecore_Rpc_Core_SubscribeTimeoutRequest()
+
+            do {
+                let call = try self.service.subscribetimeout(timeoutRequest, completion: nil)
+                while let _ = try? call.receive() {
+                    observer.on(.next(()))
+                }
+            } catch {
+                observer.onError("Failed to subscribe to timeout stream")
+            }
+            
             return Disposables.create()
         }
     }
