3a23a509235ce8d5a1cb9b0cc909c852ccc447a2
ayameMBS <marjory.silvestre@gmail.com>
2018-04-05 11:56:21 +0200

    Create sample app

---
+++ Sample Code/SwiftSampleCode/DronecodeSDKSwiftDemo/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  DronecodeSDKSwiftDemo
+//
+//  Created by Marjory Silvestre on 05.04.18.
+//  Copyright © 2018 Marjory Silvestre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
