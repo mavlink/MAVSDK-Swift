ced82008cc835596c63b63da6d111bff1dc8ae3f
ayameMBS <marjory.silvestre@gmail.com>
2018-04-05 15:45:50 +0200

    add map and tableview for telemetry entries

---
+++ Sample Code/SwiftSampleCode/DronecodeSDKSwiftDemo/Controllers/MapViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MapViewController.swift
+//  DronecodeSDKSwiftDemo
+//
+//  Created by Marjory Silvestre on 05.04.18.
+//  Copyright © 2018 Marjory Silvestre. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
