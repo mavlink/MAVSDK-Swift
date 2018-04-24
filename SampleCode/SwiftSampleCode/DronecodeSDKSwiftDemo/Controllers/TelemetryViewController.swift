0dff570f42922f901d30733b476bbf452dc70fb2
ayameMBS <marjory.silvestre@gmail.com>
2018-04-24 15:28:08 +0200

    coremanager: add singleton class to manage core system amd plugins

--- SampleCode/SwiftSampleCode/DronecodeSDKSwiftDemo/Controllers/TelemetryViewController.swift
+++ SampleCode/SwiftSampleCode/DronecodeSDKSwiftDemo/Controllers/TelemetryViewController.swift
@@ -7,21 +7,61 @@
 //
 
 import UIKit
+import Dronecode_SDK_Swift
+import RxSwift
 
 class TelemetryViewController: UIViewController {
 
     // MARK: - IBOutlets
     @IBOutlet weak var connectionLabel: UILabel!
     @IBOutlet weak var telemetryTableView: UITableView!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        connectionLabel.text = "Connection status ..."
+        connectionLabel.text = "Starting system ..."
+        
+        // Start System
+        CoreManager.shared().start()
+        
+        let coreStatus: Observable<UInt64> = CoreManager.shared().core.getDiscoverObservable()
+        _ = coreStatus.subscribe(onNext: { uuid in
+            //UUID du drone connected
+            print("Drone Discovered with UUID : \(uuid)")
+            
+                DispatchQueue.main.async { // Correct
+                    self.connectionLabel.text = "Drone Discovered with UUID : \(uuid)"
+                }
+            
+            
+            }, onError: { error in
+            print("Error Discover \(error)")
+        })
+       /* let coreTimeout: Observable<UInt64> = core.getTimeoutObservable()
+        _ = coreStatus.subscribe({  in
+            <#code#>
+        })*/
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
        
+        sleep(5)
+        
+        let position: Observable<Position> = CoreManager.shared().telemetry.getPositionObservable()
+        _ = position.subscribe(onNext: { position in
+            //print ("next pos \(position)")
+        }, onError: { error in
+            print("error telemetry")
+        })
+        
+        let health: Observable<Health> = CoreManager.shared().telemetry.getHealthObservable()
+        _ = health.subscribe(onNext: { health in
+            print ("next health \(health)")
+        }, onError: { error in
+            print("error health")
+        })
     }
 
     override func didReceiveMemoryWarning() {
