abc8e5cf781b6ce9951e79726750df6ba1feb54d
ayameMBS <marjory.silvestre@gmail.com>
2018-04-26 12:43:12 +0200

    mission: implement upload and start mission, hide pause mission button

--- SampleCode/SwiftSampleCode/DronecodeSDKSwiftDemo/Controllers/MapViewController.swift
+++ SampleCode/SwiftSampleCode/DronecodeSDKSwiftDemo/Controllers/MapViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Dronecode_SDK_Swift
 
 class MapViewController: UIViewController {
 
@@ -22,17 +23,46 @@ class MapViewController: UIViewController {
     }
     
 
-    // MARK: - IBActions
+    // MARK: - IBActions Mission
     @IBAction func sendMissionPressed(_ sender: Any) {
         print("Send Mission Pressed")
+        
+        self.uploadMission()
     }
     
     @IBAction func startMissionPressed(_ sender: Any) {
         print("Start Mission Pressed")
+        
+        // /!\ NEED TO ARM BEFORE START THE MISSION
+        let armRoutine = CoreManager.shared().action.arm()
+            .do(onError: { error in print("Arming failed")},
+                onCompleted: { self.startMission() })
+        _ = armRoutine.subscribe()
+       
     }
     
     @IBAction func pauseMissionPressed(_ sender: Any) {
         print("Pause Mission Pressed")
     }
+    
+    // MARK: - Missions
+    func uploadMission(){
+        let missionExample:ExampleMission = ExampleMission()
+        let sendMissionRoutine = CoreManager.shared().mission.uploadMission(missionItems: missionExample.missionItems).do(
+            onError: { error in print("Mission uploaded failed \(error)") },
+            onCompleted: { print("Mission uploaded with success") })
+        
+        _ = sendMissionRoutine.subscribe()
+        
+    }
+    
+    func startMission(){
+        let startMissionRoutine = CoreManager.shared().mission.startMission().do(
+            onError: { error in print("Mission started failed \(error)") },
+            onCompleted: { print("Mission started with success") })
+        
+        _ = startMissionRoutine.subscribe()
+    }
+    
 
 }
