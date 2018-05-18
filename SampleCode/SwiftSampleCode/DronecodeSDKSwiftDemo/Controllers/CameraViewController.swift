442d945d40516e631b2e8c17d38a16da4c776963
ayameMBS <marjory.silvestre@gmail.com>
2018-05-18 13:34:14 +0200

    rtsp: add Camera tab and icon

---
+++ SampleCode/SwiftSampleCode/DronecodeSDKSwiftDemo/Controllers/CameraViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CameraViewController.swift
+//  DronecodeSDKSwiftDemo
+//
+//
+
+import Foundation
+
+import UIKit
+import Dronecode_SDK_Swift
+
+class CameraViewController: UIViewController{
+    
+    // MARK: -
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+}
