69b7289bcbbb328094ae825b9c67fa5e85789f69
ayameMBS <marjory.silvestre@gmail.com>
2018-05-18 13:48:55 +0200

    rtsp: add view to show sample movie with rtsp url

--- SampleCode/SwiftSampleCode/DronecodeSDKSwiftDemo/Controllers/CameraViewController.swift
+++ SampleCode/SwiftSampleCode/DronecodeSDKSwiftDemo/Controllers/CameraViewController.swift
@@ -9,15 +9,33 @@ import Foundation
 import UIKit
 import Dronecode_SDK_Swift
 
-class CameraViewController: UIViewController{
+class CameraViewController: UIViewController, VLCMediaPlayerDelegate {
+    
+    @IBOutlet weak var cameraView: UIView!
+    var mediaPlayer = VLCMediaPlayer()
     
     // MARK: -
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        //Setup movieView
+        self.cameraView.backgroundColor = UIColor.lightGray
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         
+        //Play RTSP from web (Bunny Movie as sample)
+        //TODO modify to use camera rtsp url
+        let url = URL(string: "rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov")
+        
+        let media = VLCMedia(url: url!)
+        mediaPlayer.media = media
+        mediaPlayer.delegate = self
+        mediaPlayer.drawable = self.cameraView
+        
+        //TODO modify this
+        mediaPlayer.play()
     }
 }
