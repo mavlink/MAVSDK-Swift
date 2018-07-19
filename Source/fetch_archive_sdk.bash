22c9f8029f6bfa8041a277155605f17d1c0a94d8
Jonas Vautherin <jonas.vautherin@gmail.com>
2018-07-19 22:41:04 +0200

    use RxSwift from Cocoapods

--- Source/fetch_archive_sdk.bash
+++ Source/fetch_archive_sdk.bash
@@ -6,5 +6,5 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source ${SCRIPT_DIR}/init_variables.bash
 
-curl https://s3.eu-central-1.amazonaws.com/dronecode-sdk/${ARCHIVE_SDK} --output ${TMP_DIR}/${ARCHIVE_SDK}
-unzip -o ${TMP_DIR}/${ARCHIVE_SDK} -d ${BIN_DIR}
+curl https://s3.eu-central-1.amazonaws.com/dronecode-sdk/${ARCHIVE_SDK_CURRENT} --output ${TMP_DIR}/${ARCHIVE_SDK_CURRENT}
+unzip -o ${TMP_DIR}/${ARCHIVE_SDK_CURRENT} -d ${BIN_DIR}
