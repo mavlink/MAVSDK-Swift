20687ef098d7098652402ad6d5986b8a9152c056
Jonas Vautherin <jonas.vautherin@gmail.com>
2018-07-20 16:35:14 +0200

    restore build_grpc and build_rxswift scripts for manual install

--- Source/build_dronecode_all.bash
+++ Source/build_dronecode_all.bash
@@ -28,6 +28,8 @@ git clone https://github.com/dronecore/dronecore-swift.git ${DRONECORE_SWIFT_DIR
 mkdir -p ${DRONECORE_SWIFT_DIR}/Source/bin
 cp -r ${DRONECORE_DIR}/build/fat_bin/backend.framework ${DRONECORE_SWIFT_DIR}/Source/bin/backend.framework
 
+bash ${DRONECORE_SWIFT_DIR}/Source/build_grpc.bash
+bash ${DRONECORE_SWIFT_DIR}/Source/build_rxswift.bash
 bash ${DRONECORE_SWIFT_DIR}/Source/build_dronecode_sdk.bash
 
 # Copy resulting frameworks into ${BIN_DIR}
