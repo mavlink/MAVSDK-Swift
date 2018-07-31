15e0f5670d0904d1c8e8f15d06b17e5793a1d26e
Julian Oes <julian@oes.ch>
2018-07-31 18:06:17 +0200

    Hacky changes that allow me to build manually

--- Source/build_dronecode_sdk.bash
+++ Source/build_dronecode_sdk.bash
@@ -6,8 +6,10 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source ${SCRIPT_DIR}/init_variables.bash
 
-xcodebuild -workspace ${SCRIPT_DIR}/Dronecode-SDK-Swift/Dronecode-SDK-Swift.xcworkspace -scheme Dronecode-SDK-Swift -configuration Release -sdk iphoneos CONFIGURATION_BUILD_DIR=${BUILD_DIR}/dronecode_sdk/Release-iphoneos
-xcodebuild -workspace ${SCRIPT_DIR}/Dronecode-SDK-Swift/Dronecode-SDK-Swift.xcworkspace -scheme Dronecode-SDK-Swift -configuration Release -sdk iphonesimulator CONFIGURATION_BUILD_DIR=${BUILD_DIR}/dronecode_sdk/Release-iphonesimulator
+xcodebuild -project ${SCRIPT_DIR}/Dronecode-SDK-Swift/Dronecode-SDK-Swift.xcodeproj -target Dronecode-SDK-Swift -configuration Release -sdk iphoneos CONFIGURATION_BUILD_DIR=${BUILD_DIR}/dronecode_sdk/Release-iphoneos
+xcodebuild -project ${SCRIPT_DIR}/Dronecode-SDK-Swift/Dronecode-SDK-Swift.xcodeproj -target Dronecode-SDK-Swift -configuration Release -sdk iphonesimulator CONFIGURATION_BUILD_DIR=${BUILD_DIR}/dronecode_sdk/Release-iphonesimulator
+#xcodebuild -workspace ${SCRIPT_DIR}/Dronecode-SDK-Swift/Dronecode-SDK-Swift.xcworkspace -scheme Dronecode-SDK-Swift -configuration Release -sdk iphoneos CONFIGURATION_BUILD_DIR=${BUILD_DIR}/dronecode_sdk/Release-iphoneos
+#xcodebuild -workspace ${SCRIPT_DIR}/Dronecode-SDK-Swift/Dronecode-SDK-Swift.xcworkspace -scheme Dronecode-SDK-Swift -configuration Release -sdk iphonesimulator CONFIGURATION_BUILD_DIR=${BUILD_DIR}/dronecode_sdk/Release-iphonesimulator
 
 # Generate fat binary
 echo "Generating fat binary for Dronecode-SDK-Swift"
