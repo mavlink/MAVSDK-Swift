1ab33d34cbb7848ae608f4549040e5527f838f4f
Jonas Vautherin <jonas.vautherin@gmail.com>
2018-11-14 17:25:07 +0100

    Generation script now generates the SDK wrappers, too

--- tools/generate_from_protos.bash
+++ tools/generate_from_protos.bash
@@ -5,6 +5,7 @@ set -e
 command -v protoc || { echo >&2 "Protobuf needs to be installed (e.g. '$ brew install protobuf') for this script to run!"; exit 1; }
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PB_PLUGINS_DIR=${PB_PLUGINS_DIR:-"${SCRIPT_DIR}/../proto/pb_plugins"}
 PROTO_DIR=${PROTO_DIR:-"${SCRIPT_DIR}/../proto/protos"}
 OUTPUT_DIR=${OUTPUT_DIR:-"${SCRIPT_DIR}/../Dronecode-SDK-Swift/Generated"}
 
@@ -20,10 +21,20 @@ if [ ! -d ${OUTPUT_DIR} ]; then
     exit 1
 fi
 
+echo ""
+echo "-------------------------------"
+echo "Generating pb and grpc.pb files"
+echo "-------------------------------"
+echo ""
+
 TMP_DIR=${TMP_DIR:-"$(mktemp -d)"}
 echo "Temporary directory for this build: ${TMP_DIR}"
 
 if [ ! -d ${TMP_DIR}/grpc-swift ]; then
+    echo ""
+    echo "--- Cloning grpc-swift"
+    echo ""
+
     git -C ${TMP_DIR} clone https://github.com/grpc/grpc-swift
 fi
 
@@ -31,4 +42,25 @@ cd ${TMP_DIR}/grpc-swift && make
 
 for plugin in action camera core mission telemetry info; do
     protoc ${plugin}.proto -I${PROTO_DIR}/${plugin} --swift_out=${OUTPUT_DIR} --swiftgrpc_out=${OUTPUT_DIR} --swiftgrpc_opt=TestStubs=true --plugin=protoc-gen-swift=${TMP_DIR}/grpc-swift/protoc-gen-swift --plugin=protoc-gen-swiftgrpc=${TMP_DIR}/grpc-swift/protoc-gen-swiftgrpc
+done
+
+echo ""
+echo "-------------------------------"
+echo "Generating the SDK wrappers"
+echo "-------------------------------"
+echo ""
+
+if [ ! -d ${PB_PLUGINS_DIR}/venv ]; then
+    python3 -m venv ${PB_PLUGINS_DIR}/venv
+
+    source ${PB_PLUGINS_DIR}/venv/bin/activate
+    pip install -r ${PB_PLUGINS_DIR}/requirements.txt
+    pip install -e ${PB_PLUGINS_DIR}
+fi
+
+source ${PB_PLUGINS_DIR}/venv/bin/activate
+export TEMPLATE_PATH=${TEMPLATE_PATH:-"${SCRIPT_DIR}/../templates"}
+
+for plugin in action camera core mission telemetry; do
+    protoc ${plugin}.proto --plugin=protoc-gen-custom=$(which dcsdkgen) -I${PROTO_DIR}/${plugin} --custom_out=${OUTPUT_DIR} --custom_opt=swift
 done
