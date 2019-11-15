d7b5cd33441dd38c095fac10b4805fcbca82781a
Brian Phillips <unipheas@protonmail.com>
2019-11-15 19:36:24 +0800

    updated Packages.swift for Swift 5.1 and grpc-swift 0.10.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
@@ -7,7 +7,7 @@ let package = Package(
     .library(name: "MAVSDK_Swift", type: .dynamic, targets: ["MAVSDK-Swift"])
   ],
   dependencies: [
-    .package(url: "https://github.com/grpc/grpc-swift", .exact("0.9.1")),
+    .package(url: "https://github.com/grpc/grpc-swift", .exact("0.10.0")),
     .package(url: "https://github.com/ReactiveX/RxSwift.git", "5.0.0" ..< "6.0.0")
   ],
   targets: [
