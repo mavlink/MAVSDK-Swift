e2a63ebf3b3d9ff96b44d79f20aa4205f30a3660
Jonas Vautherin <jonas.vautherin@gmail.com>
2020-06-04 14:57:22 +0200

    Move to grpc-swift 1.0 and SPM

--- Package.swift
+++ Package.swift
@@ -7,11 +7,11 @@ let package = Package(
     .library(name: "MAVSDK_Swift", type: .dynamic, targets: ["MAVSDK-Swift"])
   ],
   dependencies: [
-    .package(url: "https://github.com/grpc/grpc-swift", .exact("0.9.1")),
+    .package(url: "https://github.com/grpc/grpc-swift", .branch("1.0.0-alpha.12")),
     .package(url: "https://github.com/ReactiveX/RxSwift.git", "5.0.0" ..< "6.0.0")
   ],
   targets: [
     .target(name: "MAVSDK-Swift",
-            dependencies: ["SwiftGRPC", "RxSwift"])
+            dependencies: ["GRPC", "RxSwift"])
   ]
 )
