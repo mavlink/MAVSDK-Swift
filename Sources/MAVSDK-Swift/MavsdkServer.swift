229f17f78817837476d49a4a6f46d1a788d474b2
Jonas Vautherin <jonas.vautherin@gmail.com>
2020-06-15 22:35:30 +0200

    update to mavsdk_server v0.28.0

--- Sources/MAVSDK-Swift/MavsdkServer.swift
+++ Sources/MAVSDK-Swift/MavsdkServer.swift
@@ -15,29 +15,30 @@ public class MavsdkServer {
      - Parameter systemAddress: The address on which `mavsdk_server` will listen for a MAVLink system. Note that this is about the MAVLink connection between `mavsdk_server` and the drone.
      - Parameter mavsdkServerPort: The port on which `mavsdk_server` will listen for a `Drone` object to connect. Note that this is about the connection between MAVSDK-Swift and `mavsdk_server`, and has nothing to do with MAVLink itself.
      */
-    public func run(systemAddress: String = "udp://:14540", mavsdkServerPort: Int = 0) {
-        self.mavsdkServerHandle = mavsdk_server.runBackend(systemAddress, Int32(mavsdkServerPort))
+    public func run(systemAddress: String = "udp://:14540", mavsdkServerPort: Int = 0) -> Int {
+        self.mavsdkServerHandle = mavsdk_server_run(systemAddress, Int32(mavsdkServerPort))
+        return Int(mavsdk_server.mavsdk_server_get_port(self.mavsdkServerHandle))
     }
 
     /**
     - Returns: The port on which `mavsdk_server` listens for connections
      */
     public func getPort() -> Int {
-        return Int(mavsdk_server.getPort(self.mavsdkServerHandle))
+        return Int(mavsdk_server_get_port(self.mavsdkServerHandle))
     }
 
     /**
      Attach to the running `mavsdk_server`, effectively blocking until it stops
      */
     public func attach() {
-        mavsdk_server.attach(self.mavsdkServerHandle!)
+        mavsdk_server_attach(self.mavsdkServerHandle!)
     }
 
     /**
      Stop `mavsdk_server`.
      */
     public func stop() {
-        mavsdk_server.stopBackend(self.mavsdkServerHandle!)
+        mavsdk_server_stop(self.mavsdkServerHandle!)
     }
 }
 #endif
