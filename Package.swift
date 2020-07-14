670dab950f71d878a0fff3026dd103365d9bd71a
Jonas Vautherin <jonas.vautherin@gmail.com>
2020-07-14 19:31:36 +0200

    Generate xcodeproj from SPM

--- Package.swift
+++ Package.swift
@@ -1,13 +1,15 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
   name: "MAVSDK-Swift",
+  platforms: [.iOS("13.5")],
   products: [
     .library(name: "MAVSDK_Swift", type: .dynamic, targets: ["MAVSDK-Swift"])
   ],
   dependencies: [
-    .package(url: "https://github.com/grpc/grpc-swift", .branch("1.0.0-alpha.12")),
+    //.package(url: "https://github.com/grpc/grpc-swift", .branch("1.0.0-alpha.16")),
+    .package(url: "https://github.com/jonasvautherin/grpc-swift", .branch("hardcode-ios-target")),
     .package(url: "https://github.com/ReactiveX/RxSwift.git", "5.0.0" ..< "6.0.0")
   ],
   targets: [
