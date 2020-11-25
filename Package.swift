1799c8cd0427369ffe9ad5454f58708f170a8ebf
Jonas Vautherin <jonas.vautherin@gmail.com>
2020-11-25 18:34:32 +0100

    Add Example, lost during master rebase

--- Package.swift
+++ Package.swift
@@ -46,6 +46,12 @@ let package = Package(
     .binaryTarget(name: "mavsdk_server",
                   url: "https://github.com/mavlink/MAVSDK/releases/download/v0.35.0/mavsdk_server.xcframework.zip",
                   checksum: "ccdd9b7c77841ea5f6b03aec8c418ed8a3bfa82770eafd296457bcfbecfbfef5"),
+    .target(name: "Example",
+            dependencies: [
+              "MAVSDK-Swift",
+              .product(name: "RxSwift", package: "RxSwift")
+            ]
+    ),
     .testTarget(name: "MAVSDK-SwiftTests",
                 dependencies: [
                   "MAVSDK-Swift",
