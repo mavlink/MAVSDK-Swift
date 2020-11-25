5cdff99f0f8f99967b1cad9b4af540da0629ff33
Jonas Vautherin <jonas.vautherin@gmail.com>
2020-11-25 19:02:25 +0100

    Fix generate_docs.sh

--- Sources/MAVSDK-Swift/tools/generate_docs.sh
+++ Sources/MAVSDK-Swift/tools/generate_docs.sh
@@ -15,8 +15,6 @@ jazzy \
     --github_url https://github.com/mavlink/MAVSDK-Swift \
     --github-file-prefix https://github.com/mavlink/MAVSDK-Swift/tree/master \
     --module-version master \
-    --xcodebuild-arguments -project,MAVSDK-Swift.xcodeproj,-scheme,MAVSDK_Swift \
-    --module MAVSDK-Swift \
     --root-url https://mavsdk.mavlink.io/docs/swift \
     --output docs/swift_output \
     --module MAVSDK_Swift
