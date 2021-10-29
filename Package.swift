2972f344ebfa90f7322b1507790b2409002e3f6d
Dmitry Malakhov <byuarus@gmail.com>
2021-10-28 17:27:01 -0700

    updated to 0.45 + distance based trigger

--- Package.swift
+++ Package.swift
@@ -47,8 +47,8 @@ let package = Package(
             ]
     ),
     .binaryTarget(name: "mavsdk_server",
-                      url: "https://github.com/byuarus/MAVSDK-XCFramework/releases/download/0.45.0-oct27/mavsdk_server.xcframework.zip",
-                      checksum: "6b4dc062a7e377db5c1e87b14a66dbdb47e4b8a59eec362f22b14f43c159c77f"),
+                      url: "https://github.com/byuarus/MAVSDK-XCFramework/releases/download/0.45.0-oct28/mavsdk_server.xcframework.zip",
+                      checksum: "6fbcdd689958c6f305417605b29552d9e1fda455663e95d034276aa967754a2f"),
     .testTarget(name: "MavsdkTests",
                 dependencies: [
                   "Mavsdk",
